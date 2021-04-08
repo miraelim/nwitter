50bbebff530327b5888fd65b5111d2bd2e775b96
mirae_lim <mirae_lim@tamx.co.kr>
2021-04-08 10:51:00 +0900

    #1.0.1 React + Firebase Setup

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAhGEkOFrzu80T76PHdkM-gqcI1l3B-_4I",
+    authDomain: "nwitter-13455.firebaseapp.com",
+    projectId: "nwitter-13455",
+    storageBucket: "nwitter-13455.appspot.com",
+    messagingSenderId: "667266176620",
+    appId: "1:667266176620:web:ff42900125fcc347bd6a49"
+  };
+
+  export default firebase.initializeApp(firebaseConfig);
