ea7c1b1202f98f6f25a59d3c812cc0fe868c0bf8
mirae_lim <mirae_lim@tamx.co.kr>
2021-04-12 10:01:47 +0900

    #6.0 Cleaning JS

--- src/components/App.js
+++ src/components/App.js
@@ -4,40 +4,38 @@ import { authService } from "fbase";
 
 function App() {
   const [init, setInit] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [userObj, setUserObj]= useState(null);
+  const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
-        setIsLoggedIn(true);
         setUserObj({
           displayName: user.displayName,
           uid: user.uid,
-          updateProfile:(args) =>user.updateProfile(args),
+          updateProfile: (args) => user.updateProfile(args),
         });
       } else {
-        setIsLoggedIn(false);
+        setUserObj(null);
       }
       setInit(true);
     });
   }, []);
-  const refreshUser = () =>{
+  const refreshUser = () => {
     const user = authService.currentUser;
     setUserObj({
       displayName: user.displayName,
-      uid:user.uid,
-      updateProfile:(args) =>user.updateProfile(args),
+      uid: user.uid,
+      updateProfile: (args) => user.updateProfile(args),
     });
   };
   return (
     <>
       {init ? (
-        <AppRouter 
-          refreshUser = {refreshUser}
-          isLoggedIn = {Boolean(userObj)}
+        <AppRouter
+          refreshUser={refreshUser}
+          isLoggedIn={Boolean(userObj)}
           userObj={userObj}
-        /> 
-      ): (
+        />
+      ) : (
         "Initializing..."
       )}
     </>
